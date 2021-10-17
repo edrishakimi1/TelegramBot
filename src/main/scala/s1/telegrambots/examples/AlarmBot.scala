5fbd01d5e169d65af1d2b397654fa16adc79215d
Kärkkäinen Eemi <eemi.karkkainen@aalto.fi>
2021-10-17 15:50:05 +0300

    Update AlarmBot.scala

--- src/main/scala/s1/telegrambots/examples/AlarmBot.scala
+++ src/main/scala/s1/telegrambots/examples/AlarmBot.scala
@@ -1,57 +1,113 @@
 package s1.telegrambots.examples
 
+import scala.collection.mutable.Buffer
+import com.bot4s.telegram.models.User
 import s1.telegrambots.BasicBot
 
-/**
-  * Tervehdysbotti, joka tervehtii kanavalle tulijoita
-  *
-  * Botti saamaan reagoimaan kanavan tapahtumiin luomalla funktio/metodi joka käsittelee
-  * halutuntyyppistä dataa ja joko palauttaa merkkijonon tai tekee jotain muuta saamallaan
-  * datalla.
-  *
-  */
-object AlarmBot extends App {
+object AppleBot extends App {
+
+  class Phone(nimi: String, val hinta: Int, val muisti: Int, val halkaisija: Double, val kamerat: Int) {
+
+    override def toString = nimi
+
+    def getMuisti = muisti
+
+    def getHinta = hinta
+
+    def getHalkaisija = halkaisija
+
+    def getKamerat = kamerat
+
+    val iPhone11 = new Phone("iPhone 11", 400,128, 6.1, 2)
+
+    val iPhone13ProMax = new Phone("iPhone 13 Pro Max", 1600, 1024, 6.7, 3)
+
+    var phones = Buffer[Phone](iPhone11, iPhone13ProMax)
+
 
   val bot = new BasicBot() {
 
-    /**
-      * Viesti käyttäjälle toivotun ajan kuluttua
-      */
-    def ajastus(message: Message): String = {
-      // Muutetaan merkkijonomme Option-käärityksi numeroksi
-      // jos tekstiä ei voi muuttaa numeroksi saadaan None.
-      val aika = util.Try(getString(message).toInt).toOption
-
-      aika match {
-        case None => "Et antanut numeroa"
-        case Some(sekunnit) => {
-
-          // Laitetaan rinnakkainen työtehtävä käyntiin
-          scala.concurrent.Future {
-            // Painetaan mieleen mistä chatistä hälytys pyydettiin
-            val chat = getChatId(message)
-            // Odotellaan hieman
-            Thread.sleep(sekunnit * 1000)
-            // Ja kirjoitellaan sinne hälytys
-            writeToChat("Hälytys!!!", chat)
-          }
-
-          // Kanavalle voidaan kuitenkin viestiä heti
-          "Ajastettiin hälytys " + sekunnit + " kuluttua."
-        }
-      }
+    def tervehdi(msg: Message) = {
+      "Hei " + getUserFirstName(msg) + ". Olet ostamassa puhelinta? Kerrotko vielä budjetin?"
     }
 
-    /**
-      * rekisteröi botille uuden toiminnon joka ajetaan kun
-      * käyttäjä kirjoittaa telegramiin esim. /ajasta 10
-      */
-    this.command("ajasta", ajastus)
+    def budjetti(message: Message): String = {
+
+      val summa = util.Try(getString(message).toInt).toOption
+
+      summa match {
+        case None => println("Sinun täytyy antaa numeroita!")
+        case Some(luku) => {
+          for (puhelin <- phones) {
+            if (puhelin.getHinta > luku) {
+              phones -= puhelin
+            }
+          }
+        }
+      }
+    "Mites muisti?"
+    }
+
+    def muisti(message: Message) = {
+
+      val summa = util.Try(getString(message).toInt).toOption
+
+      summa match {
+        case None => println("Sinun täytyy antaa numeroita!")
+        case Some(luku) => {
+          for (puhelin <- phones) {
+            if (puhelin.getMuisti != luku) {
+              phones -= puhelin
+            }
+          }
+        }
+      }
+    "Mites näytön halkaisija?"
+    }
+
+
+    def halkaisija(message: Message) = {
+
+      val summa = util.Try(getString(message).toDouble).toOption
+
+      summa match {
+        case None => println("Sinun täytyy antaa numeroita!")
+        case Some(luku) => {
+          for (puhelin <- phones) {
+            if (puhelin.getHalkaisija != luku) {
+              phones -= puhelin
+            }
+          }
+        }
+      }
+    "Mites kameroiden määrä?"
+    }
+
+
+    def kamerat(message: Message) = {
+
+      val summa = util.Try(getString(message).toInt).toOption
+
+      summa match {
+        case None => println("Sinun täytyy antaa numeroita!")
+        case Some(luku) => {
+          for (puhelin <- phones) {
+            if (puhelin.getKamerat != luku) {
+              phones -= puhelin
+            }
+          }
+        }
+      }
+    "Löytyi!"
+    }
+
+    this.command("terve", tervehdi)
+
 
     // Lopuksi Botti pitää vielä saada käyntiin
     this.run()
 
     println("Started")
-
   }
-}
+  }
+ }
