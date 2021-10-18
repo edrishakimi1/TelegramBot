30dabc16e4597f59bc3b9e5fbc51cdba133f36d0
Ahmed Hadi <ahmed.hadi@aalto.fi>
2021-10-18 09:42:35 +0300

    Nullpointer bugi korjattu. Jokaisella metodilla bufferi joka päivittyy lopuksi global bufferiin.

--- src/main/scala/s1/telegrambots/examples/AlarmBot.scala
+++ src/main/scala/s1/telegrambots/examples/AlarmBot.scala
@@ -14,7 +14,7 @@ object AppleBot extends App {
 
     def getMuisti = muisti
 
-    def getHinta = hinta
+    def getHinta: Int = hinta
 
     def getHalkaisija = halkaisija
 
@@ -24,30 +24,19 @@ object AppleBot extends App {
 
   val bot = new BasicBot() {
 
+    val iP13ProMax = new Phone("iPhone 13 Pro Max", 1600, 1024, 6.7, 3)
+    val iP13Pro = new Phone("iPhone 13 Pro", 1200,512, 6.1, 3)
+    val iP13Mini = new Phone("iPhone 13 Mini", 900,256, 5.4, 2)
+    val iP13 = new Phone("iPhone 13", 1100, 256, 6.1, 3)
+    val iP12 = new Phone("iPhone 12", 600, 128, 6.1, 2)
+    val iP12Mini = new Phone("iPhone 12 Mini", 600, 256, 5.4, 2)
+    val iP12Pro = new Phone("iPhone 12 Pro", 800, 512, 6.1, 3)
+    val iP12ProMax = new Phone("iPhone 12 Pro Max", 1200, 1024, 6.7, 3)
+    val iP11 = new Phone("iPhone 11", 400,128, 6.1, 2)
+    val iP11Pro = new Phone("iPhone 11 Pro", 600, 128, 5.8, 3)
+    val iP11ProMax = new Phone("iPhone 11 Pro Max", 900, 256, 6.4, 3)
 
-    var phones = Buffer[Phone]()
-
-    phones += (new Phone("iPhone 13 Pro Max", 1600, 1024, 6.7, 3))
-
-    phones += (new Phone("iPhone 13 Pro", 1200,512, 6.1, 3))
-
-    phones += (new Phone("iPhone 13 Mini", 900,256, 5.4, 2))
-
-    phones += (new Phone("iPhone 13", 1100, 256, 6.1, 3))
-
-    phones += (new Phone("iPhone 12", 600, 128, 6.1, 2))
-
-    phones += (new Phone("iPhone 12 Mini", 600, 256, 5.4, 2))
-
-    phones += (new Phone("iPhone 12 Pro", 800, 512, 6.1, 3))
-
-    phones += (new Phone("iPhone 12 Pro Max", 1200, 1024, 6.7, 3))
-
-    phones += (new Phone("iPhone 11", 400,128, 6.1, 2))
-
-    phones += (new Phone("iPhone 11 Pro", 600, 128, 5.8, 3))
-
-    phones += (new Phone("iPhone 11 Pro Max", 900, 256, 6.4, 3))
+    var phones = Buffer[Phone](iP13ProMax, iP13Pro, iP13Mini, iP13, iP12, iP12Mini, iP12Pro, iP12ProMax, iP11, iP11Pro, iP11ProMax)
 
     def tervehdi(msg: Message) = {
       "Hei " + getUserFirstName(msg) + "! Tervetuloa Applen puhelinkauppaan. Mikä on budjettisi puhelinta varten? (Vastaa käskyllä /budjetti)"
@@ -55,16 +44,28 @@ object AppleBot extends App {
 
     def budjetti(message: Message): String = {
 
-      val summa = util.Try(getString(message).toInt).toOption
+      val summa = getString(message).toIntOption
+      var budjettiPhones = Buffer[Phone]()
 
       summa match {
         case None => "Sinun täytyy antaa numeroita!"
         case Some(luku) => {
           for (puhelin <- phones) {
-            if (puhelin.getHinta > luku) {
-              phones -= puhelin
+            if (puhelin.getHinta <= luku) {
+              budjettiPhones += puhelin
             }
           }
+          phones = budjettiPhones
+
+      /* for (puhelin <- phones) {
+        if (puhelin.getHinta <= summa)
+          budjettiPhones += puhelin
+      }
+      phones = budjettiPhones
+
+      "Suoritettu" */
+
+
           "Paljonko muistia haluat puhelimella vähintään olevan? 128/256/512/1024 (Vastaa käskyllä /muisti)"
         }
       }
@@ -72,16 +73,18 @@ object AppleBot extends App {
 
     def muisti(message: Message) = {
 
-      val summa = util.Try(getString(message).toInt).toOption
+      val summa = getString(message).toIntOption
+      var muistiPhones = Buffer[Phone]()
 
       summa match {
         case None => "Sinun täytyy antaa numeroita!"
         case Some(luku) => {
           for (puhelin <- phones) {
-            if (puhelin.getMuisti < luku) {
-              phones -= puhelin
+            if (puhelin.getMuisti >= luku) {
+              muistiPhones += puhelin
             }
           }
+          phones = muistiPhones
           "Minkä kokoisen näytön haluat vähintään? 5.4/5.8/6.1/6.4/6.7 (Vastaa käskyllä /halkaisija)"
         }
       }
@@ -90,16 +93,18 @@ object AppleBot extends App {
 
     def halkaisija(message: Message) = {
 
-      val summa = util.Try(getString(message).toDouble).toOption
+      val summa = getString(message).toDoubleOption
+      var halkaisijaPhones = Buffer[Phone]()
 
       summa match {
         case None => "Sinun täytyy antaa numeroita!"
         case Some(luku) => {
           for (puhelin <- phones) {
-            if (puhelin.getHalkaisija < luku) {
-              phones -= puhelin
+            if (puhelin.getHalkaisija >= luku) {
+              halkaisijaPhones += puhelin
             }
           }
+          phones = halkaisijaPhones
           "Montako kameraa haluat vähintään? 1/2/3 (Vastaa käskyllä /kamerat)"
         }
       }
@@ -108,16 +113,18 @@ object AppleBot extends App {
 
     def kamerat(message: Message) = {
 
-      val summa = util.Try(getString(message).toInt).toOption
+      val summa = getString(message).toIntOption
+      var kameraPhones = Buffer[Phone]()
 
       summa match {
         case None => "Sinun täytyy antaa numeroita!"
         case Some(luku) => {
           for (puhelin <- phones) {
-            if (puhelin.getKamerat < luku) {
-              phones -= puhelin
+            if (puhelin.getKamerat >= luku) {
+              kameraPhones += puhelin
             }
           }
+          phones = kameraPhones
           "Löytyi! Käskyllä /puhelimet saat hakukriteereihisi sopivat puhelimet esille."
         }
       }
